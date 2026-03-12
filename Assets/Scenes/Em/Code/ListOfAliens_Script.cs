672abaf7f2795fac4c7f3a64198abc348e92d702
Blueberry825 <88793583+Blueberry825@users.noreply.github.com>
2026-03-12 13:46:54 +0000

    update

--- Assets/Scenes/Em/Code/ListOfAliens_Script.cs
+++ Assets/Scenes/Em/Code/ListOfAliens_Script.cs
@@ -31,7 +31,6 @@ public class ListOfAliens_Script : MonoBehaviour
         singleAlienList.Remove(alien);
 
         SaveAndLoad_scr.SaveLocationAndAlien(currentDate.GetComponent<AliensDated_Script>().alienNumber);
-        SaveAndLoad_scr.SaveLocationAndAlien(currentDate.GetComponent<AliensDated_Script>().alienNumber);
         print($"AAAAAAAAAAAAAAAAAAAAA {PlayerPrefs.GetInt("Location0")}");
 
         //remove as current date each time
