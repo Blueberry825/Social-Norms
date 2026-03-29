a7e2a3c28af1a13bd59aac5dcb48fac3b27ea2f4
emiscreating <emilysteed1874@gmail.com>
2026-03-29 15:42:01 +0100

    UI Updates
    Menu ui started to add

--- Assets/Em/Code/Menu/OpenSettings_Script.cs
+++ Assets/Em/Code/Menu/OpenSettings_Script.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class OpenSettings_Script : MonoBehaviour
 {
-    private Animator settingsAnimator;
+    [SerializeField] private Animator settingsAnimator;
 
     private void Start()
     {
