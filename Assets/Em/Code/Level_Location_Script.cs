f8c406f0b2de062bf0f5172e09fc5aefb04111c1
Blueberry825 <mail@rubyweller.co.uk>
2026-03-31 21:27:57 +0100

    small changes to map

--- Assets/Em/Code/Level_Location_Script.cs
+++ Assets/Em/Code/Level_Location_Script.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Level_Location_Script : MonoBehaviour
 {
     public int currentLocation;
+    private TMP_Text currentDateText;
     [SerializeField] List<GameObject> locationList;
 
     private void Start()
     {
+        currentDateText = GameObject.Find("CurrentMapText").GetComponent<TMP_Text>();
         currentLocation = 0;
     }
 
@@ -15,5 +18,6 @@ public class Level_Location_Script : MonoBehaviour
     { 
         currentLocation = currentLocation +1;
         Debug.Log("Current location = " + currentLocation.ToString());
+        currentDateText.text = "Current Date: " + currentLocation.ToString();
     }
 }
