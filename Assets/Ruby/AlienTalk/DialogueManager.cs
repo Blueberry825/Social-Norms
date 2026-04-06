0fc0321bb22421fb830d23e44fbebd5b178711b7
Blueberry825 <mail@rubyweller.co.uk>
2026-04-06 15:32:21 +0100

    new date trigger

--- Assets/Ruby/AlienTalk/DialogueManager.cs
+++ Assets/Ruby/AlienTalk/DialogueManager.cs
@@ -12,12 +12,16 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text dialogueText;
     private GameObject GameOver;
     private GameObject tablet;
+    private GameObject AlienList_Save;
 
     private DialogueTrigger DialogueTrigger_scr;
     public InteractionSelector InteractionSelector_scr;
     private LoveMeter LoveMeter_scr;
     private Level_Location_Script Level_Location_Script_scr;
     private TabletAppearDissapear_Script TabletAppearDissapear_Script_scr;
+    private ListOfAliens_Script ListOfAliens_Script_scr;
+    private DateRandomiser_Script DateRandomiser_Script_scr;
+
 
     public int round = 0;
     public int meLines = 0; //each round of talking ?
@@ -37,7 +41,11 @@ public class DialogueManager : MonoBehaviour
         tablet = GameObject.Find("Tablet");
         Level_Location_Script_scr = tablet.GetComponent<Level_Location_Script>();
         TabletAppearDissapear_Script_scr = tablet.GetComponent<TabletAppearDissapear_Script>();
-        
+
+        AlienList_Save = GameObject.Find("AlienList_Save");
+        ListOfAliens_Script_scr = AlienList_Save.GetComponent<ListOfAliens_Script>();
+        DateRandomiser_Script_scr = AlienList_Save.GetComponent<DateRandomiser_Script>();
+
         //get the current alien at the start of the scene// SCENE MUST START TO DATE EACH NEW ALIEN
 
         GameOver = GameObject.Find("GameOver");
@@ -86,6 +94,12 @@ public class DialogueManager : MonoBehaviour
         GameObject.Find("GameOver/Result").GetComponentInChildren<TextMeshProUGUI>().text = "Mission Complete!";
         TabletAppearDissapear_Script_scr.isLevelOver = true;//when player starts level they have no longer won
     }//tell table that hasWon is true
+
+    public void TriggerNewDate()//after date is won, trigger new date to be chosen and appear
+    {
+        DateRandomiser_Script_scr.RandomiseDate();
+        TabletAppearDissapear_Script_scr.SwapTabletVisibility();
+    }
 
     public void OptionBubbles()//switch case 
     {
