a86e48bbb38d7af55d5c5531cc7e530b20491024
Blueberry825 <mail@rubyweller.co.uk>
2026-06-01 14:41:06 +0100

    idle video play

---
+++ Assets/IdleTimer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IdleTimer : MonoBehaviour
+{
+    public int IdleTimeSetting;
+    float LastIdleTime;
+    private VideoPlayer demoClip;
+
+    void Awake()
+    {
+        demoClip = GetComponentInChildren<UnityEngine.Video.VideoPlayer>();
+        demoClip.url = "file://E:/Assignments/Major-Project/Social-Norm-Gameplay.mp4";
+        demoClip.gameObject.SetActive(false);
+        LastIdleTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey || (Input.GetAxis("Mouse Y") != 0) || (Input.GetAxis("Mouse X") != 0))
+        {
+            LastIdleTime = Time.time;
+            print("player not idle");
+            demoClip.Stop();
+            demoClip.gameObject.SetActive(false);
+        }
+
+        if (IdleCheck())
+        {
+            demoClip.gameObject.SetActive(true);
+            demoClip.Play();
+        }
+    }
+
+    public bool IdleCheck()
+    {
+        return Time.time - LastIdleTime > IdleTimeSetting;
+    }
+}
