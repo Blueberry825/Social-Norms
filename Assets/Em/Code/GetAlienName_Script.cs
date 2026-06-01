80bb173d7dd9f694b7e06a96e9fb7b87eec04159
Blueberry825 <mail@rubyweller.co.uk>
2026-06-01 15:23:13 +0100

    add bio text

--- Assets/Em/Code/GetAlienName_Script.cs
+++ Assets/Em/Code/GetAlienName_Script.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class GetAlienName_Script : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI alienName_TXT;
+    [SerializeField] private TextMeshProUGUI alienBio_TXT;
 
     private void Start()
     {
         alienName_TXT = GameObject.Find("AlienName_TXT").GetComponent<TextMeshProUGUI>();
+        alienBio_TXT = GameObject.Find("AlienBio_TXT").GetComponent<TextMeshProUGUI>();
     }
 
     public void setAlienName(string name) 
     { 
         alienName_TXT.text = name;
+    }
+
+    public void setAlienBio(string name)
+    {
+        alienBio_TXT.text = name;
     }
 }
